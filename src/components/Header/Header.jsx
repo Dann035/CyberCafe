c03046b453d3ddb468e32a2a98b4185cc92d6846
Daniel Landa <landadlh603@gmail.com>
2025-12-09 14:39:20 +0100

    emaquetado con bootstrap listo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,82 @@
-import './Header.css'
+import "./Header.css";
+import { useState } from "react";
 
 export const Header = () => {
+  const [active, setActive] = useState(false);
+  //!-----------------------------------------------------------
+  function handleIsActive(e) {
+    if (e.target.name === "Home") {
+      const isHomeActive = active ? "nav-link active" : "nav-link";
+      return;
+    } else if (e.taget.name === "Menu") {
+      const isMenuActive = active ? "nav-link active" : "nav-link";
+    }
+    setActive(!active);
+  }
+  //!-----------------------------------------------------------
   return (
-    <nav>
-      <div className="hr-box-izq">
-        <img src="" alt="" />
-        <a href="">Cyber Café</a>
-      </div>
-      <div className="hr-box-center">
-        <a href="">Link 1</a>
-        <a href="">Link 2</a>
-        <a href="">Link 3</a>
-        <a href="">Link 4</a>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary ">
+      <div className="container-fluid">
+        <a className="navbar-brand d-flex align-items-center" href="#">
+          <img
+            className="me-2 p-0"
+            src="/public/mug-hot-solid-full.svg"
+            alt="Taza de café"
+            height={"35px"}
+          />
+          Cyber-Cafe
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse ms-5"
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                name="Home"
+                aria-current="page"
+                onClick={() => handleIsActive()}
+                href="#"
+              >
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" onClick={() => handleIsActive()} href="#">
+                Menu
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                AboutUs
+              </a>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Expresso..."
+              name="search"
+            />
+            <button className="btn btn-outline-warning" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
       </div>
     </nav>
-  )
-}
+  );
+};
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,90 @@
-import './Hero.css'
+import "./Hero.css";
 
 export const Hero = () => {
   return (
     <section>
-      <img src="#" alt="imagen del hero" />
-      <nav>
-        <a href="#">Hero Link 1</a>
-        <a href="#">Hero Link 2</a>
-        <a href="#">Hero Link 3</a>
+      <div id="carouselExampleIndicators" className="carousel slide">
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="0"
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img
+              src="/public/imgHeroCafe/cafe-colando.png"
+              className="d-block w-100"
+              alt="Imagen que muestra el proceso de colada del cafe saliendo del envudo y desprendiendo el delicioso aroma"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="/public/imgHeroCafe/sirviendo-cafe.png"
+              className="d-block w-100"
+              alt="Camarero sosteniendo la taza de cafe y creando un diseño con la espuma"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="/public/imgHeroCafe/cafe-en-mesa.png"
+              className="d-block w-100"
+              alt="Una taza de café en una mesa de fondo un entorno acogedor"
+            />
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+      <nav className="nav navbar">
+        <a className="nav-link" href="#">
+          Hero Link 1
+        </a>
+        <a className="nav-link" href="#">
+          Hero Link 2
+        </a>
+        <a className="nav-link" href="#">
+          Hero Link 3
+        </a>
       </nav>
     </section>
-  )
-}
-
-
+  );
+};
